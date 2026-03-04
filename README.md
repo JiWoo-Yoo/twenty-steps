8e1e347f89ddf1ce185fdee86e2dbcd27e26ad41
JiWoo-Yoo <selfnum_1357@naver.com>
2026-03-04 16:15:50 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Twenty Steps
+
+Just a persistent piece of junk.
+
+## Tech
+
+- React (Vite)
+- CSS (Transitions & Filters)
+- Private Assets (Procreate)
---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="container">
+    Work in Progress...
+      <div className="box">
+        <div className="box-inner">
+          
+        <button onClick={()=>{
+          setCount(count + 1)
+        }}>{count}</button>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
